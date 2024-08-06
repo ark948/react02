5411e586ed4fee03c6b82740a17191a63b8d9806
ark948 <armankarimi948@gmail.com>
2024-08-06 20:24:49 +0330

    Inline styling in react

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,9 +1,14 @@
-import styles from './Button.module.css'
+// import styles from './Button.module.css'
 
 function Button() {
-    return(
-        <button className={styles.button}>Click me</button>
-    );
+    
+    const styles = {
+        backgroundColor: "hsl(200, 100&, 50%)",
+        color: "blue",
+    }
+
+    // return(<button className={styles.button}>Click me</button>);
+    return(<button style={styles}>Click me</button>);
 }
 
 export default Button
