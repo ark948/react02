7107a42b1c9c230f3a81f93650010f682967de81
ark948 <armankarimi948@gmail.com>
2024-08-09 14:17:18 +0330

    Updating the state of arrays using react hooks

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -2,26 +2,30 @@ import {useState} from 'react';
 
 function MyComponent() {
 
-    const [car, setCar] = useState({year: 2024, make: "Ford", model: "Mustang"});
+    const [foods, setFoods] = useState(["Apple", "Orange", "Banana"]);
+    // two functions, add element and delete element
 
-    function handleYearChange(event) {
-        // setCar({...car, year: event.target.value});
-        setCar(c => ({...car, year: event.target.value}));
+    function handleAddFood() {
+        const newFood = document.getElementById('foodInput').value;
+        document.getElementById('foodInput').value = '';
 
+        setFoods(f => [...f, newFood]);
     }
-    function handleMakeChange(event) {
-        setCar(c => ({...car, make: event.target.value}));
+    function handleRemoveFood(index) {
+        // make a new array consisting of all elements...
+        // except the one we chose to remove
+        // underscore is actually the element to receive but its unused
+        setFoods(foods.filter((_, i) => i !== index));
     }
-    function handleModelChange(event) {
-        setCar(c => ({...car, model: event.target.value}));
-    }
+
     return(
         <div>
-            <p>Your favorite car is: {car.year} {car.make} {car.model}</p>
-
-            <input type="number" value={car.year} onChange={handleYearChange}/><br/>
-            <input type="text" value={car.make} onChange={handleMakeChange}/><br/>
-            <input type="text" value={car.model} onChange={handleModelChange}/><br/>
+            <h2>List of Food</h2>
+            <ul>
+                {foods.map((food, index) => <li key={index} onClick={() => handleRemoveFood(index)}>{food}</li>)}
+            </ul>
+            <input type="text" id='foodInput' placeholder='Enter food name'/>
+            <button onClick={handleAddFood}>Add food</button>
         </div>
     );
 }
